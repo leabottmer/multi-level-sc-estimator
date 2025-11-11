4310231aba134e3513ff66f080fb931a2000b9f9
leabottmer <67162360+leabottmer@users.noreply.github.com>
2025-11-11 15:24:00 -0800

    Enhance synthetic control functions and lambda estimation
    Refactor synthetic control methods and optimize lambda selection.

--- multi_level_sc_estimator/mlSC.py
+++ multi_level_sc_estimator/mlSC.py
@@ -73,6 +73,47 @@ def synthetic_control(data: np.array, vals: float, t: float):
     
     return (tau_est,w_star)
 
+# Synthetic control state-county
+def obj_func_sc_sc(weights: np.array, control_vals: np.array, target: np.array):
+    data = cp.vstack((target, control_vals))
+    lambda_reg = 1e-8
+    return cp.pnorm(data[1:,:].T @ weights - data[0,:], p=2)**2+lambda_reg*cp.norm(weights)**2
+    
+def synthetic_control_counties(data: np.array, vals, t: float):
+    """
+    We use SC to find a solution.
+    """
+    
+    """
+    Synthetic Control: State-county
+    """
+  
+    # Obtain the target matrix and control matrix as well as the number of controls
+    " Define target and control "
+    target_mat = data[vals,]
+    data_small = np.delete(data, vals,axis = 0)
+    num_controls = data_small.shape[0]
+
+    # Define training and test set
+    weights = cp.Variable(num_controls)
+    X_train = data_small[:,:t]
+    Y_train = np.array([target_mat[:t]])
+    X_test = data_small[:,t:]
+    Y_test = target_mat[t:]
+
+    # Define optimization problem
+    constraints = [cp.sum(weights) == 1, 0<= weights, weights <= 1]
+    objective = cp.Minimize(obj_func_sc_sc(weights, X_train, Y_train)) # add small L2 norm to vector to ensure uniqueness
+    problem = cp.Problem(objective,constraints)
+        
+    problem.solve(solver = cp.SCS)
+
+    # Save results
+    tau_est = e_mean(X_test, Y_test, weights)
+    w_star = weights.value
+    
+    return (tau_est,w_star)
+
 # Loss function
 def loss_fn(control_vals, target, weights):
     """
@@ -186,7 +227,60 @@ def get_lambda_heuristic(var_eps: float, var_y: float):
         
     return l_h
 
-def mlSC(data_agg: np.array,data_disagg: np.array, vals: float, n_c: np.array, t: float, w_c: np.array, lambda_est: None, lambda_val: 0.0001):
+def get_lambda_cv(target_mat: np.array, control_mat: np.array, nc_c: np.array, Q: np.array, var_y: float, t_cv: float, t: float):
+    """
+    Obtain lambda through cross-validation proposed in the paper.
+
+    v
+    
+    Returns: l_cv
+    """
+
+    # Restrict data matrices to t_cv set only
+    X_train = control_mat[:,:t_cv]
+    Y_train = target_mat[:t_cv]
+    X_test = control_mat[:,t_cv:t]
+    Y_test = target_mat[t_cv:t]
+    num_controls = control_mat.shape[0]
+
+    # Define weights for the optimization problem, lambda and constraints and Q
+    weights = cp.Variable(num_controls)
+    lambd = cp.Parameter(nonneg = True)
+    constraints = [cp.sum(weights) == 1, weights >= 0] # convexity constraints
+    
+    # Initialize weights as SC solution for a warm start
+    data_state_cv = data_state[:,:t]
+    sc_state_cv = synthetic_control(data_state_cv, vals, t_cv)
+    w_sc = sc_state_cv[1]
+    weights.value = np.repeat(w_sc / nc_c, nc_c)
+
+    # Define optimization problem
+    objective = cp.Minimize(obj_func_constr(weights, X_train, Y_train, lambd, Q, var_y))
+    problem = cp.Problem(objective,constraints)
+
+    # Loop over time periods
+    cv_error = []
+    for v in lambda_grid:
+        if v ==0: # Run sc_counties if v=0
+            data_new_l = np.vstack((target_mat,control_mat))
+            data_new = data_new_l[:,:t] # reduce matrix to only pre-treatment time periods
+            sc_sc = synthetic_control_counties(data_new,0,t_cv)
+            cv_error.append(float(np.mean((Y_test-X_test.T@sc_sc[1])**2)))
+        else:
+            lambd.value = v
+            problem.solve(solver = cp.SCS)
+            cv_error.append(mse(X_test, Y_test, weights))
+    
+    # Find lambda with minimum error
+    min_index = np.argmin(cv_error) 
+    l_cv = lambda_grid[min_index] # set lambda to optimally picked lambda
+
+    return l_cv
+    
+
+    
+
+def mlSC(data_agg: np.array,data_disagg: np.array, vals: float, n_c: np.array, t: float, w_c: np.array, lambda_est = None, lambda_val = 0.0001, lambda_grid = np.concatenate(([0], np.logspace(np.log10(1e-8), np.log10(5), 50),np.logspace(np.log10(10), np.log10(1000), 5))), t_cv_periods = 1):
     """
     Finds a vector for aggregated-disaggregated data (penalized) using a heuristic or cross-validation over time.
 
@@ -198,6 +292,8 @@ def mlSC(data_agg: np.array,data_disagg: np.array, vals: float, n_c: np.array, t
     w_c: list of arrays, each array is v_s for an aggregate unit s, where v_s are the population weights for units within aggregate unit s 
     lambda_est: method to estimate lambda: "heuristic", "cross-validation" or None
     lambda_val: lambda value for penalty in case estimation method = None
+    lambda_grid: grid specified for lambda for cross-validation
+    t_cv_periods: specify the number of cross-validation periods
 
     Returns:
     tau_est: avgerage estimated treatment effect over time
@@ -243,30 +339,47 @@ def mlSC(data_agg: np.array,data_disagg: np.array, vals: float, n_c: np.array, t
     if lambda_est == "heuristic":
         l_star = get_lambda_heuristic(var_eps, var_y)
     if lambda_est == "cross-validation":
-        l_star = get_lambda_cv
+        l_star = get_lambda_cv(target_mat, control_mat, nc_c, Q, var_y, t-t_cv_periods, t)
     else:
         print("None of the estimation methods you specified for lambda are valied. Please specify "heuristic" or "cross-validation". Alternatively, if you want to feed in a single value for lambda, select None 
         and set lambda_val equal to your desired value.")
 
     " Obtain final estimates "
-    sc_state = synthetic_control(data_agg, vals, t) 
-    w_ss = sc_state[1]
-    weights.value = np.repeat(w_ss / nc_control, nc_control) # initialize weights to classical SC solution for a warm start
+
+    if l_star == 0: # if l_star =0, repeat synthetic control counties
+        data_new = np.vstack((target_mat,control_mat))
+        sc_sc = synthetic_control_counties(data_new,0,t)
+
+        # Save results
+        tau_est = sc_sc[0]
+        w_star = sc_sc[1]
+        lstar = 0
+    else:
+        " Define weights for optimization problem, lambda and constraints and block penalty matrix Q "
+        weights = cp.Variable(num_controls)
+        lambd = cp.Parameter(nonneg = True)
+        constraints = [cp.sum(weights) == 1, weights >= 0] # convexity constraints
     
-    # Define target and control matrices
-    X_train = control_mat[:,:t]
-    Y_train = target_mat[:t]
-    X_test = control_mat[:,t:]
-    Y_test = target_mat[t:] 
-    lambd.value = l_star
+        " Define warm-start "
+        sc_state = synthetic_control(data_agg, vals, t) 
+        w_ss = sc_state[1]
+        weights.value = np.repeat(w_ss / nc_control, nc_control) # initialize weights to classical SC solution for a warm start
         
-    objective = cp.Minimize(obj_func_constr(weights, X_train, Y_train, lambd, Q, var_y))
-    problem = cp.Problem(objective,constraints)
-        
-    problem.solve(solver = cp.SCS)
+        " Define target and control matrices "
+        X_train = control_mat[:,:t]
+        Y_train = target_mat[:t]
+        X_test = control_mat[:,t:]
+        Y_test = target_mat[t:] 
     
-    # Save results
-    tau_est = e_mean(X_test, Y_test, weights) # 
-    w_star = weights.value
+        " Run optimization problem "
+        
+        lambd.value = l_star    
+        objective = cp.Minimize(obj_func_constr(weights, X_train, Y_train, lambd, Q, var_y))
+        problem = cp.Problem(objective,constraints)
+        problem.solve(solver = cp.SCS)
+        
+        " Save results "
+        tau_est = e_mean(X_test, Y_test, weights) # averaged over treated period
+        w_star = weights.value
         
     return (tau_est,l_star, w_star)
