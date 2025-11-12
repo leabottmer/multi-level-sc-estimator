5dbd9b4db04692a3bddf30d2a9f003869e869d6c
leabottmer <67162360+leabottmer@users.noreply.github.com>
2025-11-11 17:32:01 -0800

    Update test_multi_level_sc_estimator.py

--- tests/test_multi_level_sc_estimator.py
+++ tests/test_multi_level_sc_estimator.py
@@ -2,7 +2,8 @@ from multi_level_sc_estimator import mlSC
 import pandas as pd
 
 # Read data
-datamat_county = pd.read_csv('ia_emp_app_teen_empl.csv')
+url = 'https://raw.githubusercontent.com/leabottmer/multi-level-sc-estimator/tests/ia_emp_app_teen_empl.csv'
+datamat_county = pd.read_csv(url)
 
 states = datamat_county.iloc[:, -1].unique()
 states = sorted(states)
